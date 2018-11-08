4f928cb797a955f444290cdff57fd61bf26b240c
Tom <lehmanno@hs-furtwangen.de>
2018-11-08 18:33:44 +0000

    comments added

--- includes/upload_to_serv.php
+++ includes/upload_to_serv.php
@@ -1,17 +1,18 @@
 <?php
 
-$gcode_pattern = "/\.(.+)gcode$/";
+$gcode_pattern = "/\.gcode$/";		// Regex Pattern zum Feststellen ob die hochzuladende Datei wirklich eine .gcode Datei ist
 
-if(!empty($_FILES['file_to_serv'])) {
-	if (preg_match($gcode_pattern, $_FILES['file_to_serv'])) {
-		$path = "uploads/gcode/";
-		$path = $path . basename( $_FILES['file_to_serv']['name']);
-		if(move_uploaded_file($_FILES['file_to_serv']['tmp_name'], $path)) {
-			echo "<div id='response'>Die Datei <b>".  basename( $_FILES['file_to_serv']['name']) . "</b> wurde hochgeladen.</div>";
+if(!empty($_FILES['file_to_serv'])) {		// Wenn Upload-Form eine Datei enthält
+	$file = $_FILES['file_to_serv']['name'];		
+	if (preg_match($gcode_pattern, $file)) {		// Prüfe Datei Endung
+		$path = "uploads/gcode/";			// Speicherort für druckbare Dateien
+		$path = $path . basename($_FILES['file_to_serv']['name']);			
+		if(move_uploaded_file($_FILES['file_to_serv']['tmp_name'], $path)) {	// Verschiebe Datei von temporären Ordner nach uploads/gcode/
+			echo "<div id='response'>Die Datei <b>".  $file . "</b> wurde hochgeladen.</div>";
 		} else{
-			echo "<div id='response'>Ein Fehler ist beim Hochladen der Datei <b>".  basename( $_FILES['file_to_serv']['name']) . " nach <b>" . $path . "</b> aufgetreten! (Keine Datei ausgewählt?)</div>";
+			echo "<div id='response'>Ein Fehler ist beim Hochladen der Datei <b>".  $file . " nach <b>" . $path . "</b> aufgetreten! (Keine Datei ausgewählt?)</div>";
 		}
 	} else {
-		echo "<div id='response'>Die Datei <b>".  basename( $_FILES['file_to_serv']['name']) . "</b> ist nicht im richtigen Format (*.gcode).</div>";
+		echo "<div id='response'>Die Datei <b>".  $file . "</b> ist nicht im richtigen Format (*.gcode).</div>";
 	}
 }
